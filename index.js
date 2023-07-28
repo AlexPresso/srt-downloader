dd0d8478551de79fd61ddd24599c8f3b3063f1c8
AlexPresso <blond.alexandre@hotmail.com>
2023-07-28 10:44:51 +0200

    Update default options

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const yargs = require('yargs');
 const SubDownloader = require('./classes/SubDownloader');
 
 const options = yargs
-    .usage('$0 -d <directory> -l <lang1,lang2,...>')
-    .option('d', {alias: "directory", describe: "videos directory path", type: "string", demandOption: false})
-    .option('l', {alias: "languages", describe: "languages (comma separated)", type: "string", demandOption: false})
+    .usage('$0 -u <username> -p <password> -a <apikey>')
+    .option('d', {alias: "directory", describe: "videos directory path", type: "string", demandOption: false, default: process.cwd()})
+    .option('l', {alias: "languages", describe: "languages (comma separated)", type: "string", demandOption: false, default: 'en'})
     .option('u', {alias: "username", describe: "OpenSubtitles.org username", type: "string", demandOption: true})
     .option('p', {alias: "password", describe: "OpenSubtitles.org password", type: "string", demandOptions: true})
     .option('a', {alias: "apiKey", describe: "OpenSubtitles.com APIKey", type: "string", demandOptions: true})
